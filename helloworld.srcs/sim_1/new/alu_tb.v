ebd1de1d4cd3e7bddae32dd1bd19933d1b54f2f2
Dylan <dylank@posteo.com>
2025-11-19 00:06:30 -0600

    simulation

---
+++ helloworld.srcs/sim_1/new/alu_tb.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company:
+// Engineer:
+//
+// Create Date: 11/18/2025 11:56:55 PM
+// Design Name:
+// Module Name: alu_tb
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module alu_tb;
+    reg [3:0] a,b,op;
+    wire [3:0] out;
+    wire cout;
+    blinky test_unit(a,b,op,out,cout);
+    initial begin
+        a=4'd1;
+        b=4'd4;
+        op = 4'h0;
+    end
+endmodule
